28aebef1f3ed76e069d3f2401684e901874476e3
Valerii Goncharenko <vgoncharenko@intellisoftware.net>
2017-10-27 12:49:09 +0300

    Refactored mongodb layer

--- src/Flumine.Mongodb/MongoDbDataStore.cs
+++ src/Flumine.Mongodb/MongoDbDataStore.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Flumine.Data;
 using Flumine.Util;
-
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
 namespace Flumine.Mongodb
@@ -13,34 +11,31 @@ namespace Flumine.Mongodb
     {
         private const string MasterId = "MASTER";
 
-        private readonly IMongoCollection<NodeDescriptor> collection;
+        private readonly IMongoCollection<NodeDescriptorEntity> collection;
 
         private Guid masterId;
 
         public MongoDbDataStore(IMongoDatabase db, string collectionName)
         {
-            collection = db.GetCollection<NodeDescriptor>(collectionName);
+            collection = db.GetCollection<NodeDescriptorEntity>(collectionName);
         }
 
         public INodeDescriptor GetMaster()
         {
-            var node = collection.Find(x => x.Id == MasterId).FirstOrDefault();
-            return node == null ? null : node.ConvertTimeToLocal();
+            return collection.Find(x => x.Id == MasterId).FirstOrDefault();
         }
 
         public bool TryTakeMasterRole(INodeDescriptor node, int deadNodeTimeout)
         {
             var deadNodeTs = ServerClock.ServerUtcNow.AddMilliseconds(-deadNodeTimeout);
 
-            var filter = Builders<NodeDescriptor>.Filter;
+            var filter = Builders<NodeDescriptorEntity>.Filter;
 
             var q = filter.And(
                 filter.Eq(x => x.Id, MasterId),
-                filter.Or(
-                    filter.Exists(x => x.NodeId, false),
-                    filter.Lt(x => x.LastSeen, deadNodeTs)));
+                filter.Lt(x => x.LastSeen, deadNodeTs));
 
-            var update = Builders<NodeDescriptor>.Update
+            var update = Builders<NodeDescriptorEntity>.Update
                 .Set(x => x.NodeId, node.NodeId)
                 .Set(x => x.Endpoints, node.Endpoints)
                 .Set(x => x.LastSeen, ServerClock.ServerUtcNow);
@@ -48,7 +43,7 @@ namespace Flumine.Mongodb
             try
             {
                 var res = collection.FindOneAndUpdate(q, update,
-                    new FindOneAndUpdateOptions<NodeDescriptor, NodeDescriptor>
+                    new FindOneAndUpdateOptions<NodeDescriptorEntity, NodeDescriptorEntity>
                     {
                         IsUpsert = true,
                         ReturnDocument = ReturnDocument.After
@@ -65,7 +60,7 @@ namespace Flumine.Mongodb
 
         public void LeaveMasterRole(INodeDescriptor node)
         {
-            var filter = Builders<NodeDescriptor>.Filter;
+            var filter = Builders<NodeDescriptorEntity>.Filter;
             var q = filter.And(
                 filter.Eq(x => x.Id, MasterId),
                 filter.Eq(x => x.NodeId, node.NodeId));
@@ -79,13 +74,13 @@ namespace Flumine.Mongodb
             var now = ServerClock.ServerUtcNow;
             collection.UpdateOne(
                 x => x.Id == node.NodeId.ToString("n"),
-                Builders<NodeDescriptor>.Update.Set(x => x.LastSeen, now));
+                Builders<NodeDescriptorEntity>.Update.Set(x => x.LastSeen, now));
 
             if (node.NodeId == masterId)
             {
                 collection.UpdateOne(
                     x => x.Id == MasterId,
-                    Builders<NodeDescriptor>.Update.Set(x => x.LastSeen, now));
+                    Builders<NodeDescriptorEntity>.Update.Set(x => x.LastSeen, now));
             }
         }
 
@@ -93,14 +88,13 @@ namespace Flumine.Mongodb
         {
             return collection.Find(x => x.Id != MasterId)
                 .ToList()
-                .Select(node => node.ConvertTimeToLocal())
                 .Cast<INodeDescriptor>()
                 .ToList();
         }
 
         public void Add(INodeDescriptor node)
         {
-            collection.InsertOne(new NodeDescriptor(node));
+            collection.InsertOne(new NodeDescriptorEntity(node));
             RefreshLastSeen(node);
         }
 
@@ -108,62 +102,5 @@ namespace Flumine.Mongodb
         {
             collection.DeleteOne(x => x.Id == node.NodeId.ToString("n"));
         }
-
-        #region Inner types
-
-        [BsonIgnoreExtraElements]
-        private class NodeDescriptor : INodeDescriptor
-        {
-            private bool isLocalTime;
-
-            public string Id { get; set; }
-
-            public Guid NodeId { get; set; }
-
-            public List<string> Endpoints { get; set; }
-
-            public DateTime LastSeen { get; set; }
-
-            public NodeDescriptor()
-            {
-            }
-
-            public NodeDescriptor(INodeDescriptor descriptor)
-            {
-                Id = GetId(descriptor.NodeId);
-                NodeId = descriptor.NodeId;
-                Endpoints = descriptor.Endpoints;
-                LastSeen = descriptor.LastSeen;
-            }
-
-            public NodeDescriptor ConvertTimeToLocal()
-            {
-                if (!isLocalTime)
-                {
-                    LastSeen = ServerClock.ToLocalUtc(LastSeen);
-                    isLocalTime = true;
-                }
-
-                return this;
-            }
-
-            public NodeDescriptor ConvertTimeToServer()
-            {
-                if (isLocalTime)
-                {
-                    LastSeen = ServerClock.ToServerUtc(LastSeen);
-                    isLocalTime = false;
-                }
-
-                return this;
-            }
-
-            public static string GetId(Guid nodeId)
-            {
-                return nodeId.ToString("n");
-            }
-        }
-
-        #endregion
     }
 }
---
+++ src/Flumine.Mongodb/NodeDescriptorEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Flumine.Data;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Flumine.Mongodb
+{
+    [BsonIgnoreExtraElements]
+    public class NodeDescriptorEntity : INodeDescriptor
+    {
+        public string Id { get; set; }
+
+        public Guid NodeId { get; set; }
+
+        public List<string> Endpoints { get; set; }
+
+        /// <summary>
+        /// Note: server time. Use ServerClock for comparison.
+        /// </summary>
+        public DateTime LastSeen { get; set; }
+
+        public NodeDescriptorEntity()
+        {
+        }
+
+        public NodeDescriptorEntity(INodeDescriptor descriptor)
+        {
+            Id = GetId(descriptor.NodeId);
+            NodeId = descriptor.NodeId;
+            Endpoints = descriptor.Endpoints;
+            LastSeen = descriptor.LastSeen;
+        }
+
+        public static string GetId(Guid nodeId)
+        {
+            return nodeId.ToString("n");
+        }
+    }
+}
